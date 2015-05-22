2fafbdf39d8523d8a4dad98fcfb934e81ec5a342
Sam Baxter <baxtersa14@gmail.com>
2015-05-21 20:31:22 -0400

    Add printf calls in interrupts for debugging

--- src/interrupts.c
+++ src/interrupts.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include "peripherals/armtimer.h"
 #include "base.h"
 #include "drivers/gpio.h"
@@ -12,27 +13,42 @@ irq_controller_t* GetIRQController(void) {
 }
 
 void __attribute__((interrupt("ABORT"))) reset_vector(void) {
-
+  printf("Reset abort interrupt...\n");
 }
 
 void __attribute__((interrupt("UNDEF"))) undefined_instruction_vector(void) {
-  while(1) {
-    // TRAP!!!
-  }
+//  while(1) {
+//    printf("Stuck in UNDEF interrupt loop!\n");
+//    // TRAP!!!
+//  }
 }
 
 void __attribute__((interrupt("SWI"))) software_interrupt_vector(void) {
-  while(1) {
-    // TRAP!!!
-  }
+//  while(1) {
+//    printf("Stuck in SWI interrupt loop!\n");
+//    // TRAP!!!
+//      static int lit = 0;
+//      
+//      // Clear the ARM Timer interrupt
+//      GetARMTimer()->m_uIRQClear = 1;
+//      
+//      // Flip the LED
+//      if (lit) {
+//          GetGPIO()->m_rGPSET1 = (1 << LED_GPIO_BIT);
+//          lit = 0;
+//      } else {
+//          GetGPIO()->m_rGPCLR1 = (1 << LED_GPIO_BIT);
+//          lit = 1;    
+//      }
+//  }
 }
 
 void __attribute__((interrupt("ABORT"))) prefetch_abort_vector(void) {
-
+  printf("Prefetch abort interrupt...\n");
 }
 
 void __attribute__((interrupt("ABORT"))) data_abort_vector(void) {
-
+  printf("Data abort interrupt...\n");
 }
 
 void __attribute__((interrupt("IRQ"))) interrupt_vector(void) {
